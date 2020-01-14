68adbe3c42bdbe434e21634fb004e290251f31b1
Per Hansen <pekkelwebsites@gmail.com>
2020-01-14 17:04:13 +0100

    Update steal.js

--- steal.js
+++ steal.js
@@ -3,9 +3,9 @@ let importString = '';
 jQuery('b:contains(Pre-BiS: )').each((index, element) => {
 	const $element = jQuery(element);
 	$element.parent().find('a').each((linkIndex, linkElement) => {
-    	const $linkElement = jQuery(linkElement);
+		const $linkElement = jQuery(linkElement);
 		const numberRegex = new RegExp(/[0-9]+/g);
-        importString += 'i:' + numberRegex.exec($linkElement.attr('href'))[0] + ',';
+		importString += 'i:' + numberRegex.exec($linkElement.attr('href'))[0] + ',';
 	});
 });
 
